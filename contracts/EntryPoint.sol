185a528702afdbe1410fad4cae0f1f259da91d85
jihwan2843 <jihwan2843@gmail.com>
2024-08-24 14:34:24 +0900

    EntryPoint Testing is almost completed

--- contracts/EntryPoint.sol
+++ contracts/EntryPoint.sol
@@ -5,23 +5,20 @@ import "./interfaces/IGrantManager.sol";
 import "./interfaces/IGrant.sol";
 import "./interfaces/IFactory.sol";
 
-contract EntryPoint{
-
+contract EntryPoint {
     IGrantManager grantmanager;
     IGrant grant;
     IFactory factory;
 
-
     uint256 private totalMatchingPool;
 
-    constructor(address _grantManagerAddr, address _factoryAddr){
+    constructor(address _grantManagerAddr, address _factoryAddr) {
         grantmanager = IGrantManager(_grantManagerAddr);
         factory = IFactory(_factoryAddr);
     }
 
-
     // 특정 그랜트에 후원하기. Grant.sol 함수를 호출하여 후원하기
-    function funding(address _addr, uint256 _amount) public returns(uint256){
+    function funding(address _addr, uint256 _amount) public returns (uint256) {
         require(_addr != address(0), "Invalid Address");
         //       후원할 그랜트의 주소
         grant = IGrant(_addr);
@@ -29,42 +26,28 @@ contract EntryPoint{
 
         return fundingAmount;
     }
-    // function funding(address _addr, uint256 _amount) public {
-    //     require(_addr != address(0), "Invalid Address");
-    //     //              후원할 그랜트의 주소
-    //     (bool success, ) = _addr.call(abi.encodeWithSignature("funding(address,uint256)", msg.sender,_amount));
-
-    //     require(success, "Funding Failed");
-    // }
 
     // Factory 함수를 호출하여 그랜트를 생성하기
-    function createGrant(string memory _title, string memory _description) public returns(address){
-        
-        address grantAddr = factory.createGrant(msg.sender, _title, _description);
+    function createGrant(
+        string memory _title,
+        string memory _description
+    ) public returns (address) {
+        address grantAddr = factory.createGrant(
+            msg.sender,
+            _title,
+            _description
+        );
 
         return grantAddr;
     }
-    // function createGrant(address _addr, string memory _title, string memory _description) public{
-    //     require(_addr != address(0), "Invalid Address");
-    //     address factoryAddress = _addr;
 
-    //     (bool success,) = factoryAddress.call(abi.encodeWithSignature("createGrant(address,string,string)",msg.sender,_title,_description));
-
-    //     require(success,"CreateGrant Failed");
-    // }
-
-    // Grant가 끝나면 관리자가 각 그랜트에 매칭풀을 비율만큼 분배 
-    function matchingDistribute() public{
-        // only administrator
-        grantmanager.setmatchingDistribute();
-
+    // Grant가 끝나면 관리자가 각 그랜트에 매칭풀을 비율만큼 분배
+    function matchingDistribute() public {
+        grantmanager.setmatchingDistribute(msg.sender);
     }
 
     // 관리자가 총 매칭 풀 금액을 설정
     function setMatchingPool(uint256 _amount) public {
-        // require 설정하기
-        grantmanager.setMatchingPool(_amount);
+        grantmanager.setMatchingPool(msg.sender, _amount);
     }
-
-
 }
---
+++ test/entryPointTest.js
@@ -0,0 +1,186 @@
+const { ethers } = require("hardhat");
+const { expect } = require("chai");
+const { extendProvider } = require("hardhat/config");
+
+describe("EntryPoint", function () {
+  let admin;
+  let owner1, addr1, addr2, addr3;
+  let owner2, addr4, addr5;
+  let owner3, addr6;
+  let grant, factory, grantManager, entryPoint;
+  let GrantManager, EntryPoint;
+  beforeEach(async function () {
+    [admin, owner1, owner2, owner3, addr1, addr2, addr3, addr4, addr5, addr6] =
+      await ethers.getSigners();
+
+    grant = await ethers.deployContract("Grant");
+    factory = await ethers.deployContract("Factory", [grant.getAddress()]);
+
+    GrantManager = await ethers.getContractFactory("GrantManager");
+    grantManager = await GrantManager.connect(admin).deploy(
+      factory.getAddress()
+    );
+
+    EntryPoint = await ethers.getContractFactory("EntryPoint");
+    entryPoint = await EntryPoint.connect(admin).deploy(
+      grantManager.getAddress(),
+      factory.getAddress()
+    );
+  });
+
+  describe("createGrant", function () {
+    it("should create a new grant", async function () {
+      // 정적으로 호출하여 반환값만 얻기
+      const grantAddr = await entryPoint
+        .connect(owner1)
+        .createGrant.staticCall("grant", "grant");
+
+      expect(grantAddr).to.be.properAddress;
+
+      await entryPoint.connect(owner1).createGrant("grant", "grant");
+      expect(await factory.getGrantbyProsper(owner1.address)).to.equal(
+        grantAddr
+      );
+
+      const newGrant = await ethers.getContractAt("Grant", grantAddr);
+      expect(await newGrant.grantProposer()).to.equal(owner1.address);
+      expect(await newGrant.grantProposer()).to.not.equal(owner2.address);
+
+      // grant 내용이 다르더라도 사용자가 2회이상 grant를 만들려고 할때 revert
+      await expect(
+        entryPoint.connect(owner1).createGrant("grant2", "grant2")
+      ).to.be.revertedWith("You have already created the grant");
+
+      // EntryPoint를 통하지 않고 Factory 컨트랙트에 직접 접근하여 grant를 만들 때는 revert
+      const tx = factory
+        .connect(owner1)
+        .createGrant(owner1.address, "grant", "grant");
+      await expect(tx).to.be.revertedWith(
+        "Direct access by EOA is not allowed"
+      );
+    });
+  });
+
+  describe("funding", function () {
+    it("should allow funding the grant when active status", async function () {
+      // grant 생성
+      const grantAddr = await entryPoint
+        .connect(owner1)
+        .createGrant.staticCall("grant", "grant");
+      await entryPoint.connect(owner1).createGrant("grant", "grant");
+
+      await ethers.provider.send("evm_increaseTime", [604810]);
+      await ethers.provider.send("evm_mine");
+
+      // 10달러 후원
+      const fundingAmount = await entryPoint
+        .connect(addr1)
+        .funding.staticCall(grantAddr, 10);
+      await entryPoint.connect(addr1).funding(grantAddr, 10);
+
+      const newGrant = await ethers.getContractAt("Grant", grantAddr);
+      expect(await newGrant.balanceOf(addr1.address)).to.equal(fundingAmount);
+    });
+
+    it("should fail when the grant is in pending status", async function () {
+      // grant 생성
+      const grantAddr = await entryPoint
+        .connect(owner1)
+        .createGrant.staticCall("grant", "grant");
+      await entryPoint.connect(owner1).createGrant("grant", "grant");
+
+      // pending일때 후원
+      const tx = entryPoint.connect(addr1).funding(grantAddr, 10);
+      await expect(tx).to.be.rejectedWith(
+        "The Status of The Grant is not Active"
+      );
+    });
+
+    it("should fail when funding with zero amount", async function () {
+      // grant 생성
+      const grantAddr = await entryPoint
+        .connect(owner1)
+        .createGrant.staticCall("grant", "grant");
+      await entryPoint.connect(owner1).createGrant("grant", "grant");
+
+      await ethers.provider.send("evm_increaseTime", [604810]);
+      await ethers.provider.send("evm_mine");
+
+      // 0달러 후원
+      const tx = entryPoint.connect(addr1).funding(grantAddr, 0);
+      await expect(tx).to.be.revertedWith("amount is zero");
+    });
+
+    it("should fail when EOA attempts to fund directly", async function () {
+      // grant 생성
+      const grantAddr = await entryPoint
+        .connect(owner1)
+        .createGrant.staticCall("grant", "grant");
+      await entryPoint.connect(owner1).createGrant("grant", "grant");
+
+      await ethers.provider.send("evm_increaseTime", [604810]);
+      await ethers.provider.send("evm_mine");
+
+      // EntryPoint를 통하지 않고 grant 컨트랙트에 직접 접근하여 funding할 때는 revert
+      const newGrant = await ethers.getContractAt("Grant", grantAddr);
+      const tx = newGrant.funding(addr1.address, 10);
+      await expect(tx).to.be.revertedWith(
+        "Direct access by EOA is not allowed"
+      );
+    });
+  });
+
+  describe("MatchingPool", function () {
+    it("should allow only admin to set the matching pool", async function () {
+      await entryPoint.connect(admin).setMatchingPool(10000);
+    });
+
+    it("should fail when non admin tries to set the matching pool", async function () {
+      const tx = entryPoint.connect(owner1).setMatchingPool(10000);
+      await expect(tx).to.be.revertedWith("You are not admin");
+    });
+  });
+
+  describe("Matching Pool Distribution", function () {
+    it("should distribute the matching pool after finishing grant", async function () {
+      // 그랜트 생성
+      const grantAddr1 = await entryPoint
+        .connect(owner1)
+        .createGrant.staticCall("grant", "grant");
+      await entryPoint.connect(owner1).createGrant("grant", "grant");
+      const grantAddr2 = await entryPoint
+        .connect(owner2)
+        .createGrant.staticCall("grant", "grant");
+      await entryPoint.connect(owner2).createGrant("grant2", "grant2");
+      const grantAddr3 = await entryPoint
+        .connect(owner3)
+        .createGrant.staticCall("grant", "grant");
+      await entryPoint.connect(owner3).createGrant("grant3", "grant3");
+      // 그랜트를 active 상태로 설정
+      await ethers.provider.send("evm_increaseTime", [604810]);
+      await ethers.provider.send("evm_mine");
+
+      // 후원하기
+      await entryPoint.connect(addr1).funding(grantAddr1, 10);
+      await entryPoint.connect(addr2).funding(grantAddr1, 10);
+      await entryPoint.connect(addr3).funding(grantAddr1, 10);
+      await entryPoint.connect(addr4).funding(grantAddr2, 20);
+      await entryPoint.connect(addr5).funding(grantAddr2, 20);
+      await entryPoint.connect(addr6).funding(grantAddr3, 30);
+
+      // 그랜트를 distributed 상태로 설정
+      await ethers.provider.send("evm_increaseTime", [2419200]);
+      await ethers.provider.send("evm_mine");
+
+      // 관리자가 매칭풀 금액을 설정
+      await entryPoint.connect(admin).setMatchingPool(10000);
+
+      const test = await ethers.getContractAt("Grant", grantAddr1);
+      const a = await test.calculateQuadraticFuding.staticCall();
+      console.log(a);
+
+      // 관리자가 매칭풀에 있는 자금을 분배
+      //await entryPoint.connect(admin).matchingDistribute();
+    });
+  });
+});
